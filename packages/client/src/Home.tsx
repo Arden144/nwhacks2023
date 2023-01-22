08db9e8a91c8483456a310811750c7719a1652de
Asar <asarmichil@gmail.com>
2023-01-22 03:38:25 -0800

    Fixed HeroButton

--- packages/client/src/Home.tsx
+++ packages/client/src/Home.tsx
@@ -1,11 +1,25 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import Button from "@lib/Button";
-import Button2 from "@lib/Button2";
 import Hero from "@lib/Hero";
 import Title from "@lib/Title";
 import Toolbar from "@lib/Toolbar";
 import { css } from "@linaria/core";
 import { Link } from "react-router-dom";
+
+const heroButt = css`
+  color: var(--secondary-label);
+  background-color: var(--background-color);
+  border: none;
+  font-size: 20px;
+  font-weight: 500;
+  padding: 1.2rem 2.5rem;
+  border-radius: 2rem;
+  margin-left: 42%;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  text-decoration: none;
+`;
 
 const heroImage = css`
   transform: translateY(-29px) scale(1.4);
@@ -29,23 +43,15 @@ const searchBar = css`
   font-size: 16px;
   margin-right: 50px;
   padding: 6px;
-  border: 1px solid #ccc;  
+  border: 1px solid #ccc;
   border-radius: 2rem;
 `;
 
 function App() {
-  const { isAuthenticated, isLoading, loginWithRedirect, logout } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
   const onLogout = () =>
     logout({ logoutParams: { returnTo: window.location.origin } });
-
-  if (isLoading) {
-    return <p>Loading</p>;
-  }
-
-  if (!isAuthenticated) {
-    return <button onClick={() => loginWithRedirect()}>Login</button>;
-  }
 
   return (
     <div>
@@ -53,23 +59,36 @@ function App() {
         <span>
           <img src="logo.svg" />
           <input className={searchBar} placeholder="Search Courses" />
-          <Link className={links} to="courses">Courses</Link>
-          <Link className={links} to="courses">Programs</Link>
-          <Link className={links} to="courses">About Us</Link>
+          <Link className={links} to="courses">
+            Courses
+          </Link>
+          <Link className={links} to="courses">
+            Programs
+          </Link>
+          <Link className={links} to="courses">
+            About Us
+          </Link>
         </span>
-        <span>
-          <Button onClick={onLogout}>Log In</Button>
-          <Button onClick={onLogout}>Sign Up</Button>
-        </span>
+        {isAuthenticated ? (
+          <Button onClick={() => logout()}>Log Out</Button>
+        ) : (
+          <span>
+            <Button onClick={() => loginWithRedirect()}>
+              Log In / Sign up
+            </Button>
+          </span>
+        )}
       </Toolbar>
       <Hero>
         <span>
           <h1 className={titleText}>
             Decentralized <br /> knowledge
           </h1>
-          <p>Learn from anyone, anywhere <br /> 
-          Uncensored learning where mentors own their content <br />
-          NFT authenitication to secure your progress</p>
+          <p>
+            Learn from anyone, anywhere <br />
+            Uncensored learning where mentors own their content <br />
+            NFT authenitication to secure your progress
+          </p>
         </span>
         <img
           className={heroImage}
@@ -79,7 +98,9 @@ function App() {
           height="385"
         />
       </Hero>
-      <Button2 onClick={onLogout}>View Courses</Button2>
+      <Link to="/courses" className={heroButt}>
+        View Courses
+      </Link>
     </div>
   );
 }
