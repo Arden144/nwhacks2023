fd8fdfef078283508ffe1ac8c70c56ff9c2da5af
Samarth Grover <samarthgrover8@gmail.com>
2023-01-22 11:43:01 -0800

    Fixed All Courses page design

---
+++ packages/client/src/lib/CourseWidget.tsx
@@ -0,0 +1,31 @@
+import { styled } from "@linaria/react";
+
+const CourseWidget = styled.div`
+    width: 250px;
+    height: 300px;
+    background-color: var(--background-color);
+    margin: 4rem 2rem;
+    border-radius: 2rem;
+    overflow: hidden;
+
+    img {
+        border-top-left-radius: 2rem;
+        border-top-right-radius: 2rem;
+        height: 58%;
+        width: 100%;
+      }
+    
+      h2 {
+        font-weight: heavy;
+        font-family: Arial;
+        padding-left: 15px;
+        padding-bottom: 0px;
+        margin: 0px;
+      }
+      p {
+        font-size: 15px;
+        padding-left: 10px;
+      }
+`;
+
+export default CourseWidget;
